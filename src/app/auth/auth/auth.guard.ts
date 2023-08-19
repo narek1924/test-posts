a4980a0a449d9783eab1e13e2d82019578868449
Narek Makaryan <nmakaryan1@gmail.com>
2023-08-19 05:55:45 +0400

    Auth guard

--- src/app/auth/auth/auth.guard.ts
+++ src/app/auth/auth/auth.guard.ts
@@ -1,35 +1,48 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+
 import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
+  CanActivateFn,
   RouterStateSnapshot,
+  Router,
   UrlTree,
 } from '@angular/router';
-import { Observable, map } from 'rxjs';
 import { AuthService } from './auth.service';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
-    return this.authService.user.pipe(
-      map((user) => {
-        const isAuth = !!user;
-        if (isAuth) {
-          return true;
-        }
-        return this.router.createUrlTree(['/auth']);
-      })
-    );
-  }
+export function authenticationGuard(): CanActivateFn {
+  return () => {
+    const oauthService: AuthService = inject(AuthService);
+    const router: Router = inject(Router);
+
+    if (!!oauthService.user) {
+      return true;
+    }
+
+    return router.createUrlTree(['/auth']);
+  };
 }
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class AuthGuard implements CanActivateFn{
+//   constructor(private authService: AuthService, private router: Router) {}
+//   canActivate(
+//     route: ActivatedRouteSnapshot,
+//     state: RouterStateSnapshot
+//   ):
+//     | Observable<boolean | UrlTree>
+//     | Promise<boolean | UrlTree>
+//     | boolean
+//     | UrlTree {
+//     return this.authService.user.pipe(
+//       map((user) => {
+//         const isAuth = !!user;
+//         if (isAuth) {
+//           return true;
+//         }
+//         return this.router.createUrlTree(['/auth']);
+//       })
+//     );
+//   }
+//   export function authenticationGuardArrow = () => inject(AuthService).isAuthenticated()
+// In this guard, we are injecting the AuthService and calling the
+// }
